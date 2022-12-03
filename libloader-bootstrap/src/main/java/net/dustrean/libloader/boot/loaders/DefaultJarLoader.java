cf1b6718233f24811cf5280bc7018d918bad5f8d
SteinGaming <danialdaryab@gmail.com>
2022-12-03 20:23:26 +0100

    refactor: Moved from Bootstrap

---
+++ libloader-bootstrap/src/main/java/net/dustrean/libloader/boot/loaders/DefaultJarLoader.java
@@ -0,0 +1,20 @@
+package net.dustrean.libloader.boot.loaders;
+
+import net.dustrean.libloader.boot.Agent;
+import net.dustrean.libloader.boot.JarLoader;
+
+import java.io.File;
+import java.net.URL;
+import java.util.jar.JarFile;
+
+public class DefaultJarLoader implements JarLoader {
+    @Override
+    public void load(URL javaFile) {
+        try {
+            Agent.appendJarFile(new JarFile(new File(javaFile.toURI())));
+        } catch (Throwable t) {
+            System.out.println("Error while loading JarFile " + javaFile);
+            t.printStackTrace(System.out);
+        }
+    }
+}
