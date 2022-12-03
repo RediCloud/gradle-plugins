e0af57ef586ad01caa6eb8b6ad240734bbd7e89c
SteinGaming <danialdaryab@gmail.com>
2022-12-03 21:58:03 +0100

    rename: Usable for both Spigot and Velocity!

--- libloader-bootstrap/src/main/java/net/dustrean/libloader/boot/loaders/URLClassLoaderJarLoader.java
+++ libloader-bootstrap/src/main/java/net/dustrean/libloader/boot/loaders/URLClassLoaderJarLoader.java
@@ -7,22 +7,18 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 /**
- * This is a JarLoader for Spigot.
- * You have to set the classLoader from your plugin to be able to use this.
- * Required is the JVM argument: --add-opens java.base/java.net=ALL-UNNAMED
- * Do it like this:
+ * This is a JarLoader for dynamic projects like <strong>plugins</strong>, which has been tested for <strong>Spigot and Velocity</strong>
+ * Required is the JVM argument: <p><strong>--add-opens java.base/java.net=ALL-UNNAMED</strong></p>
+ * You can get the classloader which is needed using:
  * <pre>
  * {@code
- * public class YourPlugin extends JavaPlugin {
- *    public void onEnable() {
- *        SpigotJarLoader.setClassLoader(((URLClassLoader) this.getClass().getClassLoader()));
- *    }
- * }
+ * URLClassLoader classLoader = (URLClassLoader) this.getClass().getClassLoader();
+ * // Both usable for Spigot and Velocity
  * }
  * </pre>
  */
-public class SpigotJarLoader implements JarLoader {
-    public SpigotJarLoader(URLClassLoader classLoader) {
+public class URLClassLoaderJarLoader implements JarLoader {
+    public URLClassLoaderJarLoader(URLClassLoader classLoader) {
         this.classLoader = classLoader;
     }
 
