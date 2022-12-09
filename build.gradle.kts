7e84286c2344c7ab228544b4673018e2f8d3599d
SteinGaming <danialdaryab@gmail.com>
2022-12-09 20:35:57 +0100

    bump: v1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
     group = "net.dustrean.libloader"
-    version = "1.3.0"
+    version = "1.3.1"
     repositories {
         mavenCentral()
         maven("https://repo.gradle.org/gradle/libs-releases/")
