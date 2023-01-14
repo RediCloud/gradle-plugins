b9fa2b70ac9c44ce2e31699b29ee676f7dc04d84
SteinGaming <danialdaryab@gmail.com>
2023-01-14 17:59:41 +0100

    fix: use add "jar:" to not use the UnixFileSystemProvider

--- libloader-bootstrap/src/main/java/net/dustrean/libloader/boot/apply/impl/JarResourceLoader.java
+++ libloader-bootstrap/src/main/java/net/dustrean/libloader/boot/apply/impl/JarResourceLoader.java
@@ -5,8 +5,11 @@ import net.dustrean.libloader.boot.apply.ResourceLoader;
 import net.dustrean.libloader.boot.model.SelfDependency;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.JarFile;
 
 public class JarResourceLoader implements ResourceLoader {
     private final String name;
@@ -16,7 +19,7 @@ public class JarResourceLoader implements ResourceLoader {
 
     public JarResourceLoader(String name, File file) {
         this.name = name;
-        Utils.Pair<List<SelfDependency>, List<String>> pair = Utils.walkThroughFS(file.toURI());
+        Utils.Pair<List<SelfDependency>, List<String>> pair = Utils.walkThroughFS(URI.create("jar:" + file.toURI()));
         dependencies = pair.first();
         repositories = pair.second();
     }
